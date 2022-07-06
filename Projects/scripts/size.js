1a48de5545aed66efe002e1e34d13193452fc540
Jeremy Glebe <jeremy.glebe@christushealth.org>
2022-07-06 14:38:35 -0500

    removed projects folder (moved to its own repo)

--- Projects/scripts/size.js
+++
@@ -1,3 +0,0 @@
-function isSmall() { return window.innerWidth <= 640 }
-function isMedium() { return window.innerWidth > 640 && window.innerWidth <= 1007 }
-function isLarge() { return window.innerWidth > 1007 }
